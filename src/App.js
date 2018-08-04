d9160fcd8868fec19b13e310290837c7271e6b3c
Ana Todorova <todorova@outlook.com>
2018-08-04 12:38:49 +0300

    set about as default

--- src/App.js
+++ src/App.js
@@ -1,45 +1,39 @@
-import React, { Component } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Route, Switch} from 'react-router-dom';
 import './styles/css/App.css';
 // import Bootstrap from 'react-bootstrap';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-import Menu from './componentMenu/Menu.jsx'
-import About from './componentMenu/About.jsx'
-import Work from './componentMenu/Work.jsx'
-import Education from './componentMenu/Education.jsx'
-import Hobby from './componentMenu/Hobby.jsx'
-import Skills from './componentMenu/Skills.jsx'
-import Projects from './componentMenu/Projects.jsx'
-import Linked from './componentMenu/Linkedin.jsx'
-import Github from './componentMenu/Github.jsx'
-import Facebook from './componentMenu/Facebook.jsx'
-
+import Menu from './componentMenu/Menu.jsx';
+import About from './componentMenu/About.jsx';
+import Work from './componentMenu/Work.jsx';
+import Education from './componentMenu/Education.jsx';
+import Hobby from './componentMenu/Hobby.jsx';
+import Skills from './componentMenu/Skills.jsx';
+import Projects from './componentMenu/Projects.jsx';
+import Linked from './componentMenu/Linkedin.jsx';
+import Github from './componentMenu/Github.jsx';
+import Facebook from './componentMenu/Facebook.jsx';
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className="App">
-      <Menu/>
-      {/* <About/>
-      <Work/>
-      <Education/> */}
-      {/* <Hobby/>
-      <Skills/>
-      <Projects/> */}
-      <Linked/>
-      <Github/>
-      <Facebook/>
+        <Menu />
+        <Linked />
+        <Github />
+        <Facebook />
 
-      <Switch>
-        <Route path='/about' component={About} />
-        <Route path='/education' component={Education} />
-        <Route path='/work' component={Work} />
-        <Route path='/skills' component={Skills} />
-        <Route path='/projects' component={Projects} />
-        <Route path='/hobby' component={Hobby} />
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route path="/about" component={About} />
+          <Route path="/education" component={Education} />
+          <Route path="/work" component={Work} />
+          <Route path="/skills" component={Skills} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/hobby" component={Hobby} />
         </Switch>
-        
+
       </div>
     );
   }
--- src/componentMenu/About.jsx
+++ src/componentMenu/About.jsx
@@ -1,46 +1,45 @@
 import React, {Component} from 'react';
-import ProfilImage from '../styles/profil2.png'
+import ProfilImage from '../styles/profil2.png';
 
-class About extends Component{
-constructor() {
-        super();
+class About extends Component {
+  constructor () {
+    super ();
+  }
 
-    }
+  render () {
+    return (
+      <div id="wrapper">
 
-    render(){
-        return(
-         <div id='wrapper'>
+        <img id="profilImg" src={ProfilImage} alt="" />
+        <h1>Ana Todorova</h1>
+        <h2>Junior Front-end Developer</h2>
 
-            <img id='profilImg' src={ProfilImage} alt=""/>
-            <h1>Ana Todorova</h1>
-            <h2>Junior Front-end Developer</h2>
-
-            <div id='textAbout'>
-                <p>
-                I am dynamic girl who has finished only half marathon but with ambition, desire and skill to finish the full marathon.
-                </p>
-                <p>
-                I have already built good social skills and work ethics. I have always worked and loved to work in a team.
-                </p>
-                <p>In my free time, I like to learn new things, to be active and to do something different from the usual everyday
-                life.
-                </p>
-            </div>
-
-            <div id='contacts'>
-                <ul>
-                <li>email:
-                <a href="">todorova@outlook.com</a>
-                </li>
-                <li>phone: +359 884 928 484</li>
-                <li>skype: ana_todorova11</li>
-                </ul>
-            </div>
-
+        <div id="textAbout">
+          <p>
+            I am dynamic girl who has finished only half marathon but with ambition, desire and skill to finish the full marathon.
+          </p>
+          <p>
+            I have already built good social skills and work ethics. I have always worked and loved to work in a team.
+          </p>
+          <p>
+            In my free time, I like to learn new things, to be active and to do something different from the usual everyday
+            life.
+          </p>
         </div>
 
+        <div id="contacts">
+          <ul>
+            <li>
+              email:
+              <a href=""> todorova@outlook.com</a>
+            </li>
+            <li>phone: +359 884 928 484</li>
+            <li>skype: ana_todorova11</li>
+          </ul>
+        </div>
 
-        )
-    }
+      </div>
+    );
+  }
 }
-export default About
+export default About;
